2a6e33381f0b6a040a7f19a8ce168280dddc2c66
Yossi Miller <y0504199881@gmail.com>
2024-09-19 10:20:54 +0300

    init project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+# הגבלת גישה לפי כמות בקשות
+from flask_limiter import Limiter
+# מחלק גישה לכל לקוח ולא כללי
+from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
